346444755edfc082289fef0cff7925dc57038f2d
robertlagrasse <robert.lagrasse@gmail.com>
2016-09-12 19:12:53 -0700

    Pushing again. Last commit did something funky.

--- app/src/main/java/com/example/android/popularmovies/DisplayActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DisplayActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class DisplayActivity extends AppCompatActivity {
 
@@ -15,5 +18,11 @@ public class DisplayActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String value = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         Log.e("DisplayActivity.class", value);
+
+        ImageView poster = (ImageView) findViewById(R.id.poster);
+
+        Picasso.with(this)
+                .load("http://www.gstatic.com/tv/thumb/movieposters/4363/p4363_p_v8_aa.jpg")
+                .into(poster);
     }
 }
