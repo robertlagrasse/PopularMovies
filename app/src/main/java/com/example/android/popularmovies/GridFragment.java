e9a15d360b0875e879910ff2b1ff71f5a62cb8a7
robertlagrasse <robert.lagrasse@gmail.com>
2016-09-15 10:04:58 -0700

    GridFragment - GridFragment is now populated through online data via ArrayAdapter. OnItemClick is logging everything included in the movieobject. Will pass this information to the next task shortly.
    MovieObject.java - Fully defines all of the attributes a movie can have, according to TMDB. Broke down the full JSON so this class ensures maximum utility if I want to use it later.

--- app/src/main/java/com/example/android/popularmovies/GridFragment.java
+++ app/src/main/java/com/example/android/popularmovies/GridFragment.java
@@ -3,8 +3,10 @@ package com.example.android.popularmovies;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Movie;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -19,6 +21,8 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,6 +34,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by robert on 9/10/16.
@@ -38,51 +43,39 @@ import java.util.ArrayList;
  */
 public class GridFragment extends Fragment {
 
-    private ArrayAdapter<String> mMovieAdapter;
-
-    // Interface reference variable
     Communicator communicator;
+    ArrayList<MovieObject> theHopper;
+    ImageAdapter imageAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.grid_fragment, container, false);
 
-        // Point my Interface reference variable at the activity that spawned this fragment.
         communicator = (Communicator) getActivity();
-
-        // Build object to fetch movie data
-        FetchMoviesTask fetch = new FetchMoviesTask();
-        // Launch the AsyncTask's doInBackground
-        // The AsyncTask will download the data and run it through a JSONParser
-        // doInBackground will then return, which kicks off AsyncTask onPostExecute().
-        // onPostExecute populates the
-
-        fetch.execute("Shaft!");
-
-        mMovieAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(), // The current context (this activity)
-                        R.layout.grid_fragment, // The name of the grid.
-                   //     R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                        new ArrayList<String>());
+        // Build a place for a bunch of movie objects to wait for something to do
+        theHopper = new ArrayList<>();
 
 
+        // Go build the movie objects and put them in theHopper
+        updateMovies();
 
-        // inflate the gridview, set ImageAdapter on it to populate with some dummy images
         GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
-        gridview.setAdapter(mMovieAdapter);
+        imageAdapter = new ImageAdapter(getActivity(), R.id.gridview, theHopper);
+        gridview.setAdapter(imageAdapter);
 
-        // TODO : set image adapter on gridview
-
-        // Listen for user interaction.
-        // For now, drop something in the logs.
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view,
                                     int position, long id) {
-                Log.e("GridFragment()", "Item Clicked at Position: " + position);
-                // use the interface to pass position data to the respond method in the
-                // activity the interface reference variable points to.
+                Log.e("OnItemClick", theHopper.get(position).getMovie_poster_path());
+                Log.e("OnItemClick", theHopper.get(position).getMovie_adult());
+                Log.e("OnItemClick", theHopper.get(position).getMovie_backdrop_path());
+                Log.e("OnItemClick", theHopper.get(position).getMovie_genre_ids());
+                Log.e("OnItemClick", theHopper.get(position).getMovie_id());
+                Log.e("OnItemClick", theHopper.get(position).getMovie_original_language());
+                Log.e("OnItemClick", theHopper.get(position).getMovie_original_title());
+                Log.e("OnItemClick", theHopper.get(position).getMovie_overview());
+                Log.e("OnItemClick", theHopper.get(position).getMovie_release_date());
                 communicator.respond(position);
             }
         });
@@ -90,69 +83,54 @@ public class GridFragment extends Fragment {
         return rootView;
     }
 
-//    // adapted from https://developer.android.com/guide/topics/ui/layout/gridview.html
-//    public class ImageAdapter extends BaseAdapter {
-//        private Context mContext;
-//
-//        public ImageAdapter(Context c) {
-//            mContext = c;
-//        }
-//
-//        public int getCount() {
-//            return mThumbIds.length;
-//        }
-//
-//        public Object getItem(int position) {
-//            return null;
-//        }
-//
-//        public long getItemId(int position) {
-//            return 0;
-//        }
-//
-//        // create a new ImageView for each item referenced by the Adapter
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            ImageView imageView;
-//            if (convertView == null) {
-//                // if it's not recycled, initialize some attributes
-//                imageView = new ImageView(mContext);
-//                imageView.setLayoutParams(new GridView.LayoutParams(240, 240));
-//                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-//                imageView.setPadding(8, 8, 8, 8);
-//            } else {
-//                imageView = (ImageView) convertView;
-//            }
-//
-//            imageView.setImageResource(mThumbIds[position]);
-//            return imageView;
-//        }
-//
-//        // references to our images
-//        private Integer[] mThumbIds = {
-//                R.drawable.sample_2, R.drawable.sample_3,
-//                R.drawable.sample_4, R.drawable.sample_5,
-//                R.drawable.sample_6, R.drawable.sample_7,
-//                R.drawable.sample_0, R.drawable.sample_1,
-//                R.drawable.sample_2, R.drawable.sample_3,
-//                R.drawable.sample_4, R.drawable.sample_5,
-//                R.drawable.sample_6, R.drawable.sample_7,
-//                R.drawable.sample_0, R.drawable.sample_1,
-//                R.drawable.sample_2, R.drawable.sample_3,
-//                R.drawable.sample_4, R.drawable.sample_5,
-//                R.drawable.sample_6, R.drawable.sample_7
-//        };
-//    }
+    // adapted from https://developer.android.com/guide/topics/ui/layout/gridview.html
+    public class ImageAdapter extends ArrayAdapter<MovieObject> {
+        private Context mContext;
+        private ArrayList<MovieObject> mMovies;
+        private int mResource;
 
-    public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
+
+        public ImageAdapter(Context context, int resource, ArrayList<MovieObject> objects) {
+            super(context, resource, objects);
+            mContext = context;
+            mMovies = objects;
+        }
+
+        public int getCount() {
+            return mMovies.size();
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+
+            MovieObject movie = getItem(position);
+
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                // imageView.setLayoutParams(new GridView.LayoutParams(240, 240));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setAdjustViewBounds(true);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            String baseurl = "http://image.tmdb.org/t/p/w185";
+            Picasso.with(getContext()).load(baseurl.concat(movie.getMovie_poster_path())).into(imageView);
+            return imageView;
+        }
+    }
+
+    public class FetchMoviesTask extends AsyncTask<String, Void, ArrayList<MovieObject>> {
 
         private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
 
-        // doInBackground()
-        // This method is called when an AsyncTask object calls its
-        // .execute() method.
-        //
         @Override
-        protected String[] doInBackground(String... strings) {
+        protected ArrayList<MovieObject> doInBackground(String... strings) {
 
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
@@ -181,8 +159,6 @@ public class GridFragment extends Fragment {
                         .build();
 
                 URL url = new URL(builtUri.toString());
-
-                Log.e("TMDB-QMF", builtUri.toString());
 
                 // Create the request, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -239,44 +215,83 @@ public class GridFragment extends Fragment {
         }
 
 
-        // This method takes raw Json input and extracts values
-        // It's just a placeholder for now
-        private String[] reformatMovieData(String rawInput)
+        private ArrayList<MovieObject> reformatMovieData(String rawInput)
                 throws JSONException {
 
             // These are the JSON elements we need to extract
 
-            final String POPULARTIY         = "popularity";
-            final String POSTER_PATH        = "poster_path";
-            final String RELEASE_DATE       = "release_date";
-            final String TITLE              = "title";
-            final String VOTE_AVERAGE       = "vote_average";
+            final String MOVIE_RESULTS              = "results";
+            final String MOVIE_POSTER_PATH          = "poster_path";
+            final String MOVIE_ADULT                = "adult";
+            final String MOVIE_OVERVIEW             = "overview";
+            final String MOVIE_RELEASE_DATE         = "release_date";
+            final String MOVIE_GENRE_IDS            = "genre_ids";
+            final String MOVIE_ID                   = "id";
+            final String MOVIE_ORIGINAL_TITLE       = "original_title";
+            final String MOVIE_ORIGINAL_LANGUAGE    = "original_language";
+            final String MOVIE_TITLE                = "title";
+            final String MOVIE_BACKDROP_PATH        = "backdrop_path";
+            final String MOVIE_POPULARITY           = "popularity";
+            final String MOVIE_VOTE_COUNT           = "vote_count";
+            final String MOVIE_VIDEO                = "video";
+            final String MOVIE_VOTE_AVERAGE         = "vote_average";
 
-            JSONObject movieJSON = new JSONObject(rawInput);
-            JSONArray movieArray = movieJSON.getJSONArray("results");
+            // Build a JSONObject from the rawInput. This is going to be multiple
+            //entries at this point.
+            JSONObject blobOfJSON = new JSONObject(rawInput);
 
-            String[] extracted = new String[movieArray.length()];
-            for(int i = 0; i < movieArray.length(); i++) {
+            // Split the JSON object up into an array, Keyed on "results"
+            JSONArray movieJSONArray = blobOfJSON.getJSONArray(MOVIE_RESULTS);
 
-                // Build a String array out of the movie objects.
-                JSONObject movie = movieArray.getJSONObject(i);
-                extracted[i] = movie.toString();
+            //Create the ArrayList we'll need to return
+            ArrayList<MovieObject> movies = new ArrayList<>();
+
+            // Iterate through the JSON array
+            for(int i=0;i<movieJSONArray.length();i++){
+                // Build an object to look at this JSON
+                JSONObject tempJSON = movieJSONArray.getJSONObject(i);
+                // Build an empty MovieObject
+                MovieObject tempMovie = new MovieObject();
+
+                // Populate the MovieObject with the JSON data
+                tempMovie.setMovie_poster_path(tempJSON.getString(MOVIE_POSTER_PATH));
+                tempMovie.setMovie_adult(tempJSON.getString(MOVIE_ADULT));
+                tempMovie.setMovie_overview(tempJSON.getString(MOVIE_OVERVIEW));
+                tempMovie.setMovie_release_date(tempJSON.getString(MOVIE_RELEASE_DATE));
+                tempMovie.setMovie_genre_ids(tempJSON.getString(MOVIE_GENRE_IDS));
+                tempMovie.setMovie_id(tempJSON.getString(MOVIE_ID));
+                tempMovie.setMovie_original_title(tempJSON.getString(MOVIE_ORIGINAL_TITLE));
+                tempMovie.setMovie_original_language(tempJSON.getString(MOVIE_ORIGINAL_LANGUAGE));
+                tempMovie.setMovie_title(tempJSON.getString(MOVIE_TITLE));
+                tempMovie.setMovie_backdrop_path(tempJSON.getString(MOVIE_BACKDROP_PATH));
+                tempMovie.setMovie_popularity(tempJSON.getString(MOVIE_POPULARITY));
+                tempMovie.setMovie_vote_count(tempJSON.getString(MOVIE_VOTE_COUNT));
+                tempMovie.setMovie_video(tempJSON.getString(MOVIE_VIDEO));
+                tempMovie.setMovie_vote_average(tempJSON.getString(MOVIE_VOTE_AVERAGE));
+
+                // Add the populated movie to the ArrayList we're going to return
+                movies.add(tempMovie);
             }
 
-            // This returns to doInBackground, which immediately returns this result
-            return extracted;
+            return movies;
         }
 
         @Override
-        protected void onPostExecute(String[] result) {
-            Log.e("onPostExecute", result[0]);
+        protected void onPostExecute(ArrayList<MovieObject> movies) {
+            theHopper.clear();
 
+            if(movies!=null){
 
-
-
-
-
+                for(MovieObject a : movies){
+                    theHopper.add(a);
+                }
+            imageAdapter.notifyDataSetChanged();
+            }
         }
     }
 
+    private void updateMovies(){
+        FetchMoviesTask fetch = new FetchMoviesTask();
+        fetch.execute("Shaft!");
+    }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/MovieObject.java
@@ -0,0 +1,136 @@
+package com.example.android.popularmovies;
+
+/**
+ * Created by robert on 9/14/16.
+ */
+public class MovieObject {
+
+
+    public String movie_poster_path;
+    public String movie_adult;
+    public String movie_overview;
+    public String movie_release_date;
+    public String movie_genre_ids;
+    public String movie_id;
+    public String movie_original_title;
+    public String movie_original_language;
+    public String movie_title;
+    public String movie_backdrop_path;
+    public String movie_popularity;
+    public String movie_vote_count;
+    public String movie_video;
+    public String movie_vote_average;
+
+    public String getMovie_poster_path() {
+        return movie_poster_path;
+    }
+
+    public String getMovie_adult() {
+        return movie_adult;
+    }
+
+    public String getMovie_overview() {
+        return movie_overview;
+    }
+
+    public String getMovie_release_date() {
+        return movie_release_date;
+    }
+
+    public String getMovie_genre_ids() {
+        return movie_genre_ids;
+    }
+
+    public String getMovie_id() {
+        return movie_id;
+    }
+
+    public String getMovie_original_title() {
+        return movie_original_title;
+    }
+
+    public String getMovie_original_language() {
+        return movie_original_language;
+    }
+
+    public String getMovie_title() {
+        return movie_title;
+    }
+
+    public String getMovie_backdrop_path() {
+        return movie_backdrop_path;
+    }
+
+    public String getMovie_popularity() {
+        return movie_popularity;
+    }
+
+    public String getMovie_vote_count(){
+        return movie_vote_count;
+    }
+
+    public String getMovie_video() {
+        return movie_video;
+    }
+
+    public String getMovie_vote_average() {
+        return movie_vote_average;
+    }
+
+    public void setMovie_poster_path(String new_value) {
+        movie_poster_path = new_value;
+    }
+
+    public void setMovie_adult(String new_value) {
+        movie_adult = new_value;
+    }
+
+    public void setMovie_overview(String new_value) {
+        movie_overview = new_value;
+    }
+
+    public void setMovie_release_date(String new_value) {
+        movie_release_date = new_value;
+    }
+
+    public void setMovie_genre_ids(String new_value) {
+        movie_genre_ids = new_value;
+    }
+
+    public void setMovie_id(String new_value) {
+        movie_id = new_value;
+    }
+
+    public void setMovie_original_title(String new_value) {
+        movie_original_title = new_value;
+    }
+
+    public void setMovie_original_language(String new_value) {
+        movie_original_language = new_value;
+    }
+
+    public void setMovie_title(String new_value) {
+        movie_title = new_value;
+    }
+
+    public void setMovie_backdrop_path(String new_value) {
+        movie_backdrop_path = new_value;
+    }
+
+    public void setMovie_popularity(String new_value) {
+        movie_popularity = new_value;
+    }
+
+    public void setMovie_vote_count(String new_value){
+        movie_vote_count = new_value;
+    }
+
+    public void setMovie_video(String new_value) {
+        movie_video = new_value;
+    }
+
+    public void setMovie_vote_average(String new_value) {
+        movie_vote_average = new_value;
+    }
+
+}
